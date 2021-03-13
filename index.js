df9fe89a0b76f133d835ff4248b489d52296cd94
AlexSyrov <1854105@gmail.com>
2021-03-13 19:59:50 +0200

    Финал

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import imgs from './gallery-items.js';
 
 
-const list = document.querySelector('.gallery');
+const list = document.querySelector('.js-gallery');
+
+
+// part #1
 
 imgs.forEach(el => {
-    const { preview, original, description } = el;
-    list.insertAdjacentHTML('beforeend',`<li class="gallery__item">
+  const { preview, original, description } = el;
+  list.insertAdjacentHTML('beforeend', `<li class="gallery__item">
   <a
     class="gallery__link"
     href="${original}"
@@ -18,5 +21,49 @@ imgs.forEach(el => {
     />
   </a>
 </li>`
-    )
-})
+  )
+});
+
+// part #2
+
+list.addEventListener('click', clickOnImg);
+
+function clickOnImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  } else { 
+    const target = event.target;
+    console.log(target.dataset.source);
+    modalIsOpen();
+    srcInModal(event);
+  }
+};
+
+// part #3
+
+const modal = document.querySelector('.js-lightbox');
+function modalIsOpen () {
+  modal.classList.add('is-open');
+};
+
+// part #4
+
+const imgInModal = document.querySelector(".lightbox__image");
+function srcInModal(event) {
+  imgInModal.src = event.target.dataset.source;
+  imgInModal.alt = event.target.alt;
+};
+
+// part #5
+
+const modalBtnClose = document.querySelector('button');
+modalBtnClose.addEventListener('click', closeModal);
+function closeModal() {
+  modal.classList.remove('is-open');
+  // part #6
+  imgInModal.src = '';
+}
+
+
+
